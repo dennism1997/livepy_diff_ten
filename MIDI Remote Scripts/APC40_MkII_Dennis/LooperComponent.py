dd5fd59b396c22ec5192811cac2f2151adda70d9
dennism1997 <dennismouwen@gmail.com>
2021-06-18 13:49:18 +0200

    Finish Looper

--- MIDI Remote Scripts/APC40_MkII_Dennis/LooperComponent.py
+++ MIDI Remote Scripts/APC40_MkII_Dennis/LooperComponent.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
+
+# noinspection PyUnresolvedReferences
+import Live
+
 from _Framework.ButtonElement import ButtonElement
 from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
+from _Framework.Dependency import depends
+from _Framework.Util import nop
 
 
 def quantize(num):
@@ -11,17 +17,18 @@ class LooperComponent(ControlSurfaceComponent):
     """Handles looping controls"""
     __module__ = __name__
 
-    def __init__(self, parent, *a, **k):
+    @depends(show_message=nop)
+    def __init__(self, parent, show_message=nop, *a, **k):
         super(LooperComponent, self).__init__(*a, **k)
-
+        self._show_message = show_message
         self._parent = parent
         self._loop_in_button = None
         self._loop_out_button = None
         self._loop_double_button = None
         self._loop_halve_button = None
-        self._clip_length = 0
         self._shift_button = None
         self._current_clip = None
+        self.loop_start = 0
         self._shift_pressed = False
 
     def get_loop_length(self):
@@ -35,46 +42,50 @@ class LooperComponent(ControlSurfaceComponent):
         assert ((button is None) or (isinstance(button, ButtonElement) and button.is_momentary()))
         if self._loop_in_button != button:
             if self._loop_in_button is not None:
-                self._loop_in_button.remove_value_listener(self.start_loop)
+                self._loop_in_button.remove_value_listener(self.loop_in)
             self._loop_in_button = button
             if self._loop_in_button is not None:
-                self._loop_in_button.add_value_listener(self.start_loop)
+                self._loop_in_button.add_value_listener(self.loop_in)
 
-    def start_loop(self, value):
+    def loop_in(self, value):
         # toggles loop, sets start point to the current playing position
         if value > 0:
             self.get_current_clip()
-            if self._current_clip is not None:
-                current_clip = self._current_clip
-                if current_clip.looping == 1:
-                    current_clip.looping = 0
+            clip = self._current_clip
+            if clip is not None:
+                self.loop_start = quantize(clip.playing_position)
+                # loop_end = quantize(clip.end_marker)
+                # clip.looping = 1
+                # clip.loop_start = self.loop_start
+                # clip.loop_end = quantize(clip.end_marker)
+
+    def loop_out(self, value):
+        # sets end point to the current playing position
+        if value > 0:
+            self.get_current_clip()
+            clip = self._current_clip
+            if clip is not None:
+                if clip.looping == 0:
+                    clip.looping = 1
+                    clip.loop_start = 0
+                    clip.loop_end = clip.end_marker
+                    clip.loop_start = self.loop_start
+
+                    loop_end = quantize(clip.playing_position)
+                    clip.loop_end = loop_end
+                    self._show_message(u"{0}, {1}".format(self.loop_start, loop_end))
+
                 else:
-                    if self._clip_length == 0:
-                        self._clip_length = current_clip.length
-                    current_position = current_clip.playing_position
-                    current_clip.looping = 1
-                    # set end to the end of the song for now
-                    current_clip.loop_end = quantize(self._clip_length)
-                    # set start to the current position
-                    current_clip.loop_start = quantize(current_position)
+                    clip.looping = 0
 
     def set_loop_off_button(self, button):
         assert ((button is None) or (isinstance(button, ButtonElement) and button.is_momentary()))
         if self._loop_out_button != button:
             if self._loop_out_button is not None:
-                self._loop_out_button.remove_value_listener(self.stop_loop)
+                self._loop_out_button.remove_value_listener(self.loop_out)
             self._loop_out_button = button
             if self._loop_out_button is not None:
-                self._loop_out_button.add_value_listener(self.stop_loop)
-
-    def stop_loop(self, value):
-        # sets end point to the current playing position
-        if value > 0:
-            self.get_current_clip()
-            if self._current_clip is not None:
-                current_clip = self._current_clip
-                current_position = current_clip.playing_position
-                current_clip.loop_end = quantize(current_position)
+                self._loop_out_button.add_value_listener(self.loop_out)
 
     def set_loop_double_button(self, button):
         assert ((button is None) or (isinstance(button, ButtonElement) and button.is_momentary()))
@@ -147,5 +158,5 @@ class LooperComponent(ControlSurfaceComponent):
 
     def _shift_value(self, value):  # added
         assert (self._shift_button is not None)
-        assert (value in range(128))
+        assert (0 <= value <= 127)
         self._shift_pressed = (value != 0)
